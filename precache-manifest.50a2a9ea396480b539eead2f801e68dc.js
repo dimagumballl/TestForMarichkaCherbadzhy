0f14bed86503618ac6342337abcdf4b2ded5268f
unknown <dimagumball@gmail.com>
2020-09-05 18:03:14 +0300

    Updates

---
+++ precache-manifest.50a2a9ea396480b539eead2f801e68dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f08b3dbc340e1d4f7a6ea577946dbc0b",
+    "url": "/TestForMarichkaCherbadzhy/index.html"
+  },
+  {
+    "revision": "4ff9db6181074f28da8c",
+    "url": "/TestForMarichkaCherbadzhy/static/css/main.4f43a4ef.chunk.css"
+  },
+  {
+    "revision": "b7b254561634adc33329",
+    "url": "/TestForMarichkaCherbadzhy/static/js/2.3dd2a6b7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TestForMarichkaCherbadzhy/static/js/2.3dd2a6b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ff9db6181074f28da8c",
+    "url": "/TestForMarichkaCherbadzhy/static/js/main.b1f386e3.chunk.js"
+  },
+  {
+    "revision": "cf40184bafd20fef2a38",
+    "url": "/TestForMarichkaCherbadzhy/static/js/runtime-main.52278fc0.js"
+  }
+]);
